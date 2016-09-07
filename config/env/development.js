f60e835e92cdd66d761826fb9580712b80fdf4cc
Di Qiu <qiudilucky@gmail.com>
2016-09-06 20:29:29 -0400

    change configuration to connect local mongodb

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,12 @@
 'use strict';
 
+var connect_string = 'mongodb://localhost:27017/meandemo';
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connect_string = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.OPENSHIFT_APP_NAME;
+}
+
 module.exports = {
-  db: 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.OPENSHIFT_APP_NAME,
+  db: connect_string,
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
